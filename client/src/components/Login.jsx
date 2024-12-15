56b4f52143931406f6a78e1b98fa5ed0d729d83a
SUDIP10K89 <sudip10k89@gmail.com>
2024-12-15 18:57:30 +0545

    Adding login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      setMessage(response.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message || "Invalid Credentials!");
+    }
+  };
   return (
     <div>
       <h1>Login</h1>
-      <form action="">
-        <input type="text" placeholder='Username'/>
-        <input type="password" name="" id="" placeholder='Password'/>
+      <form onSubmit={handleSubmit}>
+      <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <button type='submit'>Login</button>
       </form>
+      {message && <p>{message}</p>}
     </div>
   )
 }
