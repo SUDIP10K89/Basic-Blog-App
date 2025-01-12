1d82afff0b4cc306bc6a901d8dc9e283cec16f00
SUDIP10K89 <sudip10k89@gmail.com>
2025-01-12 18:55:49 +0545

    er

--- server/client/src/components/Home.jsx
+++ server/client/src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/posts");
+        const response = await axios.get("https://blog-app-4j8r.onrender.com/api/posts");
         setBlogs(response.data);
       } catch (err) {
         setError("Failed to fetch blogs. Please try again later.");
