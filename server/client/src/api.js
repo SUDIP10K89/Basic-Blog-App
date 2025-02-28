88ecfab18d044d10e3840af164ea92aa5ae6a960
SUDIP10K89 <sudip10k89@gmail.com>
2025-02-28 08:45:38 +0545

    Update API base URL to production environment

--- server/client/src/api.js
+++ server/client/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    // baseURL:'https://blog-app-4j8r.onrender.com',
-    baseURL:'http://localhost:5000',
+    baseURL:'https://blog-app-4j8r.onrender.com',
+    // baseURL:'http://localhost:5000',
 });
 
 export default api;
